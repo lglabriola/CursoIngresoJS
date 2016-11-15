05bcf0fe21dff6ef96d01a1d8cabe1c9d108c0b6
lglabriola <lucas.labriola@gmail.com>
2016-11-15 17:56:06 -0300

    3er Commit ejercicios del 6 al 10
    15112016

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	//id="importe"
+	//id="resultado"
+	var importe;
+	var resultado;
+	
+
+	//declaro variable con Id
+	importe=document.getElementById('importe').value;
+
+	//Solucion 1 -
+	resultado=parseInt(importe)*0.75;
+
+	
+	document.getElementById('resultado').value=resultado;
 	
 }
